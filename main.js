884a9ce399e13a8e996e88d20fa3cec493ed6e7f
kimhart <kim@jwplayer.com>
2020-09-15 18:27:07 -0400

    toggle created, grabbing test elements, styled labels

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
-var testIds = '[data-qa], [data-test]';
+const testIds = '[data-test]';
+const highlightClass = 'scout-qa--highlight';
 
 function toggleHighlight(e) {
-  const on = e.target.checked;
+  const targets = Array.from(document.querySelectorAll(testIds));
+  const toggle = document.querySelector('.scout-qa--toggle-slider');
+
+  if (e.target.checked) {
+    targets.map((target) => {
+      const toggle = document.querySelector('.scout-qa--toggle-slider');
+      toggle.textContent = "ON";
+
+      const container = document.createElement('div');
+      const valueWrap = document.createElement('div');
+      const labelWrap = document.createElement('div');
+
+      const label = document.createTextNode("data-test");
+      const value = target.dataset.test;
+
+      container.classList.add('scout-qa--label-wrap');
+      container.append(labelWrap);
+      container.append(valueWrap);
+
+      labelWrap.append(label);
+      valueWrap.append(value);
+
+      labelWrap.classList.add('scout-qa--label');
+      valueWrap.classList.add('scout-qa--value');
+      
+      target.appendChild(container);
+      target.classList.add(highlightClass);
+    })
+  } else {
+    targets.map((target) => {
+      const container = document.querySelector('.scout-qa--label-wrap');
+      toggle.textContent = "OFF";
+      container.remove();
+      target.classList.remove(highlightClass);
+    })
+  }
 }
 
-document.getElementById('scout-qa--toggle').addEventListener('click', toggleHighlight);
+document.getElementById('scout-toggle').addEventListener('click', toggleHighlight);
