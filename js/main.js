1a3463ba202e87d3f8edeca654fee03570418d30
kimhart <kim@jwplayer.com>
2020-09-16 18:27:01 -0400

    add polyfill, new style with count

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
-const testIds = '[data-qa], [data-test]';
-const highlightClass = 'scout-qa--highlight';
-
 function toggleHighlight(e) {
+  const testIds = '[data-qa], [data-test]';
+  const highlightClass = 'scout-qa--highlight';
   const targets = Array.from(document.querySelectorAll(testIds));
   const toggle = document.querySelector('.scout-qa--toggle-slider');
+  const description = document.querySelector('.scout-qa--description');
 
   if (e.target.checked) {
-    targets.map((target) => {
+    targets.forEach(target => {
+
       const toggle = document.querySelector('.scout-qa--toggle-slider');
       const idName = Object.keys(target.dataset)[0];
-      toggle.textContent = "ON";
+      toggle.textContent = "SHOW";
+
+      if (targets.length === 0) {
+        description.textContent = `No test IDs found`;
+      } else {
+        description.textContent = `Scout found: ${targets.length} test IDs`;
+      }
 
       const container = document.createElement('div');
       const valueWrap = document.createElement('div');
@@ -27,18 +34,26 @@ function toggleHighlight(e) {
 
       labelWrap.classList.add('scout-qa--label');
       valueWrap.classList.add('scout-qa--value');
-      
+
       target.appendChild(container);
       target.classList.add(highlightClass);
     })
   } else {
-    targets.map((target) => {
+    targets.forEach(target => {
       const container = document.querySelector('.scout-qa--label-wrap');
-      toggle.textContent = "OFF";
+      toggle.textContent = "HIDE";
+      description.textContent = 'Hit the switch to highlight test IDs'
       container.remove();
       target.classList.remove(highlightClass);
     })
   }
 }
 
-document.getElementById('scout-toggle').addEventListener('click', toggleHighlight);
+
+document.getElementById('scout-toggle').addEventListener('click', (e) => {
+  toggleHighlight(e);
+  browser.tabs.executeScript(null, {
+    code: `toggleHighlight(e)`
+  })
+});
+
